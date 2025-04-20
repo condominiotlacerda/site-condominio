4f9c4cf6c25181e531539faea835399dc04daf9a
Condomínio Edifício T Lacerda <condominiotlacerda@gmail.com>
2025-04-20 19:45:23 -0300

    Add files via upload

---
+++ inject-env.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+
+const indexHtmlPath = path.join(__dirname, 'index.html');
+let indexHtmlContent = fs.readFileSync(indexHtmlPath, 'utf8');
+
+indexHtmlContent = indexHtmlContent.replace(
+  /"VITE_FIREBASE_API_KEY_PLACEHOLDER"/g,
+  `"${process.env.FIREBASE_API_KEY}"`
+);
+indexHtmlContent = indexHtmlContent.replace(
+  /"VITE_FIREBASE_AUTH_DOMAIN_PLACEHOLDER"/g,
+  `"${process.env.FIREBASE_AUTH_DOMAIN}"`
+);
+indexHtmlContent = indexHtmlContent.replace(
+  /"VITE_FIREBASE_DATABASE_URL_PLACEHOLDER"/g,
+  `"${process.env.FIREBASE_DATABASE_URL}"`
+);
+indexHtmlContent = indexHtmlContent.replace(
+  /"VITE_FIREBASE_PROJECT_ID_PLACEHOLDER"/g,
+  `"${process.env.FIREBASE_PROJECT_ID}"`
+);
+indexHtmlContent = indexHtmlContent.replace(
+  /"VITE_FIREBASE_STORAGE_BUCKET_PLACEHOLDER"/g,
+  `"${process.env.FIREBASE_STORAGE_BUCKET}"`
+);
+indexHtmlContent = indexHtmlContent.replace(
+  /"VITE_FIREBASE_MESSAGING_SENDER_ID_PLACEHOLDER"/g,
+  `"${process.env.FIREBASE_MESSAGING_SENDER_ID}"`
+);
+indexHtmlContent = indexHtmlContent.replace(
+  /"VITE_FIREBASE_APP_ID_PLACEHOLDER"/g,
+  `"${process.env.FIREBASE_APP_ID}"`
+);
+indexHtmlContent = indexHtmlContent.replace(
+  /"VITE_FIREBASE_MEASUREMENT_ID_PLACEHOLDER"/g,
+  `"${process.env.FIREBASE_MEASUREMENT_ID}"`
+);
+
+fs.writeFileSync(indexHtmlPath, indexHtmlContent, 'utf8');
+
+console.log('Variáveis de ambiente injetadas no index.html');
