ab27ed4a419896ec8b695a0030f6c6dec81d08be
Condomínio Edifício T Lacerda <condominiotlacerda@gmail.com>
2025-04-22 15:24:15 -0300

    Update logger.js

--- netlify/functions/logger.js
+++ netlify/functions/logger.js
@@ -1,3 +1,19 @@
+const admin = require('firebase-admin');
+
+// Inicialize o Firebase Admin SDK fora do handler para reutilização
+let isFirebaseInitialized = false;
+if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://logsite-d81dd-default-rtdb.firebaseio.com' // Substitua pela sua URL do Realtime Database
+  });
+  isFirebaseInitialized = true;
+  console.log('Firebase Admin SDK foi inicializado.');
+} else {
+  console.log('Firebase Admin SDK já estava inicializado.');
+}
+
 exports.handler = async (event) => {
   if (event.httpMethod === 'OPTIONS') {
     return {
@@ -13,7 +29,19 @@ exports.handler = async (event) => {
 
   if (event.httpMethod === 'POST') {
     try {
-      console.log("Função log-access foi chamada com POST!");
+      console.log("Função logger foi chamada com POST!");
+      console.log('Corpo da requisição:', event.body);
+      const logData = JSON.parse(event.body);
+      const db = admin.database();
+      const logsRef = db.ref('logs');
+      console.log('Referência ao nó de logs obtida:', logsRef.toString());
+
+      // Adiciona um timestamp do servidor aos dados do log
+      logData.serverTimestamp = admin.database.ServerValue.TIMESTAMP;
+      console.log('Dados do log antes de gravar:', logData);
+
+      const newLogRef = await logsRef.push(logData);
+      console.log('Log registrado com sucesso no Realtime Database com ID:', newLogRef.key);
 
       return {
         statusCode: 200,
@@ -21,17 +49,17 @@ exports.handler = async (event) => {
           'Access-Control-Allow-Origin': '*',
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ message: "Log registrado com sucesso (simulado)" }),
+        body: JSON.stringify({ message: "Log registrado com sucesso no Realtime Database!" }),
       };
     } catch (error) {
-      console.error("Erro na função log-access:", error);
+      console.error("Erro ao processar a função logger:", error);
       return {
         statusCode: 500,
         headers: {
           'Access-Control-Allow-Origin': '*',
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ error: "Erro ao registrar o log" }),
+        body: JSON.stringify({ error: `Erro ao registrar o log no Realtime Database: ${error}` }),
       };
     }
   } else {
