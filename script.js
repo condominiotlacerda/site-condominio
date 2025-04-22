61de77f2c62158eb372604260796a391a653aed0
Condomínio Edifício T Lacerda <condominiotlacerda@gmail.com>
2025-04-22 15:13:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -494,7 +494,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // Ajuste para horário de Brasília (UTC-3)
 window.logAccess = function (userCode, downloadedFile, apartment) {
-  fetch('/.netlify/functions/logger', {
+  fetch('https://brilliant-gumption-dac373.netlify.app/.netlify/functions/logger', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
