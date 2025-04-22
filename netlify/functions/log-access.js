67a7d0bb164375e42e83c00ee53609203c9aa7a9
Condomínio Edifício T Lacerda <condominiotlacerda@gmail.com>
2025-04-22 14:32:22 -0300

    Update log-access.js

--- netlify/functions/log-access.js
+++ netlify/functions/log-access.js
@@ -1,16 +1,23 @@
 exports.handler = async (event) => {
-  try {
-    console.log("Função log-access foi chamada!");
+  if (event.httpMethod === 'POST') {
+    try {
+      console.log("Função log-access foi chamada com POST!");
 
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Log registrado com sucesso (simulado)" }),
+      };
+    } catch (error) {
+      console.error("Erro na função log-access:", error);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: "Erro ao registrar o log" }),
+      };
+    }
+  } else {
     return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Log registrado com sucesso (simulado)" }),
-    };
-  } catch (error) {
-    console.error("Erro na função log-access:", error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: "Erro ao registrar o log" }),
+      statusCode: 405,
+      body: JSON.stringify({ error: "Método não permitido" }),
     };
   }
 };
