370682b3530a13e5a9e25531ab209f94e4b78b88
Condomínio Edifício T Lacerda <condominiotlacerda@gmail.com>
2025-04-22 15:29:51 -0300

    Update logger.js

--- netlify/functions/logger.js
+++ netlify/functions/logger.js
@@ -1,17 +1,12 @@
 const admin = require('firebase-admin');
 
 // Inicialize o Firebase Admin SDK fora do handler para reutilização
-let isFirebaseInitialized = false;
 if (!admin.apps.length) {
   const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: 'https://logsite-d81dd-default-rtdb.firebaseio.com' // Substitua pela sua URL do Realtime Database
   });
-  isFirebaseInitialized = true;
-  console.log('Firebase Admin SDK foi inicializado.');
-} else {
-  console.log('Firebase Admin SDK já estava inicializado.');
 }
 
 exports.handler = async (event) => {
@@ -30,18 +25,21 @@ exports.handler = async (event) => {
   if (event.httpMethod === 'POST') {
     try {
       console.log("Função logger foi chamada com POST!");
-      console.log('Corpo da requisição:', event.body);
       const logData = JSON.parse(event.body);
       const db = admin.database();
       const logsRef = db.ref('logs');
-      console.log('Referência ao nó de logs obtida:', logsRef.toString());
+
+      const now = new Date();
+      const formattedDateTime = now.toISOString().replace('T', '_').replace(/:/g, '-').split('.')[0];
+      const aptoNumber = logData.apartment.replace('apto', '');
+      const safeFileName = logData.downloadedFile.replace(/[^a-zA-Z0-9_-]/g, '_');
+      const logKey = `${aptoNumber}_${formattedDateTime}_${safeFileName}`;
 
       // Adiciona um timestamp do servidor aos dados do log
       logData.serverTimestamp = admin.database.ServerValue.TIMESTAMP;
-      console.log('Dados do log antes de gravar:', logData);
 
-      const newLogRef = await logsRef.push(logData);
-      console.log('Log registrado com sucesso no Realtime Database com ID:', newLogRef.key);
+      await logsRef.child(logKey).set(logData);
+      console.log('Log registrado com sucesso no Realtime Database com chave:', logKey);
 
       return {
         statusCode: 200,
@@ -49,7 +47,7 @@ exports.handler = async (event) => {
           'Access-Control-Allow-Origin': '*',
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ message: "Log registrado com sucesso no Realtime Database!" }),
+        body: JSON.stringify({ message: "Log registrado com sucesso no Realtime Database com chave personalizada!" }),
       };
     } catch (error) {
       console.error("Erro ao processar a função logger:", error);
