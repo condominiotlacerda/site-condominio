605e528bf48a1353cbf0af2627b29100ae9345f7
Condomínio Edifício T Lacerda <condominiotlacerda@gmail.com>
2025-06-17 17:45:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -148,7 +148,7 @@ export function showFiles(apartment) {
                     event.preventDefault();
                     // *** É AQUI QUE VOCÊ PRECISA ADICIONAR O LOG ***
                     const apartmentId = localStorage.getItem('apartmentId');
-                    const notificationText = this.textContent;
+                    const notificationText = this.textContent.replace(/\./g, '_').replace(/\n/g, '_');
                     logAccess(null, `Visualização da notificação: ${notificationText}`, apartmentId);
                     openFileViewer(filename);
                   });
