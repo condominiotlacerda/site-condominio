7e1b84d81ccd441a15a65766dbc5cb877a3694e6
Condomínio Edifício T Lacerda <condominiotlacerda@gmail.com>
2025-06-18 13:56:18 -0300

    Update logger.js

--- netlify/functions/logger.js
+++ netlify/functions/logger.js
@@ -60,7 +60,7 @@ exports.handler = async (event) => {
         }
 
         const prefixo = visualizado + (tipoDocumento ? '' : arquivo);
-        logKey = `${aptoNumber}_${userName}_${formattedDateTime}_${prefixo}${tipoDocumento || 'arquivo'}_${nomeArquivo}_apto_${aptoNumber}_pdf`;
+        logKey = `${aptoNumber}_${userName}_${formattedDateTime}_${prefixo}${tipoDocumento || ''}_${nomeArquivo}_apto_${aptoNumber}_pdf`;
       } else if (logData.avisoNr) { // Lógica para os avisos entendidos
         const avisoNr = logData.avisoNr;
         logKey = `${aptoNumber}_${userName}_${formattedDateTime}_${visualizado}aviso_${avisoNr}_apto_${aptoNumber}_pdf`;
