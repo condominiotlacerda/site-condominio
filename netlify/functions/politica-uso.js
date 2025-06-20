f84e9dceb7232fa7c02c6eaed5e9f3be6e6b7602
Condomínio Edifício T Lacerda <condominiotlacerda@gmail.com>
2025-06-20 13:09:33 -0300

    Update politica-uso.js

--- netlify/functions/politica-uso.js
+++ netlify/functions/politica-uso.js
@@ -1,11 +1,30 @@
 const { google } = require('googleapis');
 
 exports.handler = async (event) => {
-  const fileId = process.env.GOOGLE_DRIVE_POLITICA_USO_ID; // Usando variável de ambiente
-  const apiKey = process.env.GOOGLE_DRIVE_API_KEY; // Usando variável de ambiente
+  const configDriveId = process.env.GOOGLE_DRIVE_CONFIGURACOES_ID; // ID do configuracoes.json no Drive
+  const apiKey = process.env.GOOGLE_DRIVE_API_KEY; // Sua API Key do Google Drive
 
   try {
     const drive = google.drive({ version: 'v3', auth: apiKey });
+
+    // Primeiro, vamos buscar o arquivo configuracoes.json do Google Drive
+    const configResponse = await drive.files.get({
+      fileId: configDriveId,
+      alt: 'media'
+    });
+
+    if (configResponse.status !== 200) {
+      console.error('Erro ao obter o arquivo configuracoes.json do Google Drive:', configResponse);
+      return {
+        statusCode: configResponse.status,
+        body: JSON.stringify({ error: 'Erro ao obter o arquivo configuracoes.json do Google Drive' }),
+      };
+    }
+
+    const configData = await configResponse.data.json();
+    const fileId = configData.politicas.politica_uso; // Obtém o ID da Política de Uso do configuracoes.json
+
+    // Agora, vamos buscar o arquivo da Política de Uso usando o ID obtido
     const response = await drive.files.get({
       fileId: fileId,
       alt: 'media', // Solicita o conteúdo real do arquivo
@@ -24,14 +43,14 @@ exports.handler = async (event) => {
         isBase64Encoded: true, // Indica que o corpo da resposta está codificado em base64
       };
     } else {
-      console.error('Erro ao obter o arquivo do Google Drive:', response);
+      console.error('Erro ao obter o arquivo da Política de Uso do Google Drive:', response);
       return {
         statusCode: response.status,
-        body: JSON.stringify({ error: 'Erro ao obter o arquivo do Google Drive' }),
+        body: JSON.stringify({ error: 'Erro ao obter o arquivo da Política de Uso do Google Drive' }),
       };
     }
   } catch (error) {
-    console.error('Erro na função:', error);
+    console.error('Erro na função ao acessar o Google Drive:', error);
     return { statusCode: 500, body: JSON.stringify({ error: error.message }) };
   }
 };
