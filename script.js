ca1350059d31a8ffb2f739bf79015f14ce4dd68c
ubcthx <condominiotlacerda@gmail.com>
2025-07-20 15:38:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -236,7 +236,8 @@ async function loadBoletos(apartmentId) {
     const responseConfig = await fetch('/dados/configuracoes.json');
     const configData = await responseConfig.json();
     console.log("Dados dos boletos do configData:", configData.boletos);
-    const boletosApartamento = configData.boletos[`${apartmentId}`]; // Usando o ID do apartamento diretamente (sem "apto_")
+    const apartmentNumber = apartmentId.replace('apto', '');
+    const boletosApartamento = configData.boletos[`apto_${apartmentNumber}`]; // Usando o ID do apartamento diretamente (sem "apto_")
     console.log("Dados de boletosApartamento (JSON):", JSON.stringify(boletosApartamento, null, 2));
     const boletos = Object.entries(boletosApartamento || {})
       .filter(([name]) => name !== '')
